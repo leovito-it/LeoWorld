02ab1fb0185e6ff7dae4020df788ae4f04fd0f5b
leovito-it <88047706+leovito-it@users.noreply.github.com>
2021-11-01 17:36:51 +0700

    Delete and change location path
    Some scripts and sprites be changed

---
+++ Assets/Scripts/AI/AutoMap.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMap : MonoBehaviour
+{
+    TileGrid tileGrid;
+    Sprite block_left;
+    Sprite block_right;
+    Sprite block_up;
+    Sprite block_down;
+}
